ae2d28dbcabde2a28fa49097504e3b4fd3c5adce
pchchv <ipchchv@gmail.com>
2023-04-08 10:32:12 +0300

    Implement a New func

--- ut.go
+++ ut.go
@@ -1,6 +1,8 @@
 package ut
 
 import (
+	"fmt"
+	"strings"
 	"time"
 )
 
@@ -9,4 +11,14 @@ type Time struct {
 	Timestamp int64
 	Zone      string
 	UTC       time.Time
+}
+
+func New() (t Time) {
+	t.Now = time.Now()
+	t.Timestamp = t.Now.Unix()
+	t.UTC = t.Now.UTC()
+	zone, _ := t.Now.Zone()
+	t.Zone = zone + " " + strings.Split(fmt.Sprint(t.Now), " ")[2]
+
+	return
 }
